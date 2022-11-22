5285a79060f2df26bcc42c0c55a47983626ff199
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2022-11-22 13:33:00 +0100

    README

--- src/call.js
+++ src/call.js
@@ -1,6 +1,6 @@
 // Vonage API key can be public. Secret cannot.
 // https://stackoverflow.com/questions/60064861/opentok-toxbox-keep-the-api-key-secret
-// When using Daily's OpenTok shim, 
+// When using Daily's OpenTok shim,
 // the API key is not used and can be set to anything.
 const apiKey = '47542751';
 
